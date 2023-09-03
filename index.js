894306ee9fa45bec00db4957c62aa99de38d0ac6
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-09-04 01:39:03 +0600

    modify code

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// Enable  middleware
+// Enable middleware
 app.use(cors());
 app.use(express.json());
 
 const dbuser = "test-crud";
 const dbpass = "dKxyHDKaiYni9rd2";
-
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
 
 const uri = `mongodb+srv://${dbuser}:${dbpass}@cluster0.dp3om9f.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,37 +22,56 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Define your route handlers here
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// Connect to MongoDB and start the server
 async function run() {
   try {
     // Connect the client to the server
-    // await client.connect();
+    await client.connect();
     const TaskCollection = client.db("TaskCollection").collection("tasks");
-    // task get api
-    app.get("/getTasks", async (req, res) => {
-      const result = await TaskCollection.find().toArray();
-      res.send(result);
-    });
-    // post request for task
-    app.post("/tasks", async (req, res) => {
-      const data = req.body;
-      try {
-        // Insert the data into MongoDB
-        const result = await TaskCollection.insertOne(data);
-        res.send(result);
-      } catch (error) {
-        console.error("Error creating data:", error);
-        res.status(500).json({ message: "Internal server error" });
-      }
-    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(" You successfully connected to MongoDB!");
+    console.log("You successfully connected to MongoDB!");
+
+    // Start listening on the defined port
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+
   } finally {
+    // Note: You should close the MongoDB client when done
     // await client.close();
   }
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-  run().catch(console.dir);
+// Define your route handlers here
+// Task get API
+app.get("/getTasks", async (req, res) => {
+  const TaskCollection = client.db("TaskCollection").collection("tasks");
+  const result = await TaskCollection.find().toArray();
+  res.send(result);
 });
+
+// Post request for tasks
+app.post("/tasks", async (req, res) => {
+  const TaskCollection = client.db("TaskCollection").collection("tasks");
+  const data = req.body;
+  try {
+    // Insert the data into MongoDB
+    const result = await TaskCollection.insertOne(data);
+    res.send(result);
+  } catch (error) {
+    console.error("Error creating data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Run the application
+run().catch(console.dir);

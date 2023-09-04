8667c62e65824adab841e4b45a726e781b2783a8
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-09-04 21:33:57 +0600

    added all round crud

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const bodyParser = require("body-parser");
 // Enable middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 const dbuser = "test-crud";
 const dbpass = "dKxyHDKaiYni9rd2";
@@ -35,7 +36,8 @@ async function run() {
     // Connect the client to the server
     await client.connect();
     const TaskCollection = client.db("TaskCollection").collection("tasks");
-    
+    // started api from here
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
@@ -44,14 +46,13 @@ async function run() {
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
-
   } finally {
     // Note: You should close the MongoDB client when done
     // await client.close();
   }
 }
 
-// Define your route handlers here
+// Define route handlers here
 // Task get API
 app.get("/getTasks", async (req, res) => {
   const TaskCollection = client.db("TaskCollection").collection("tasks");
@@ -59,8 +60,29 @@ app.get("/getTasks", async (req, res) => {
   res.send(result);
 });
 
+// Task get API by ID
+app.get("/getTask/:id", async (req, res) => {
+  const itemId = req.params.id; 
+
+  const TaskCollection = client.db("TaskCollection").collection("tasks");
+
+  try {
+    const result = await TaskCollection.findOne({ _id: new ObjectId(itemId) });
+    console.log(result)
+    if (!result) {
+      // If no task with the provided ID is found, return a 404 response
+      return res.status(404).send("Task not found");
+    }
+
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching task:", error);
+    res.status(500).send("Error fetching task");
+  }
+});
+
 // Post request for tasks
-app.post("/tasks", async (req, res) => {
+app.post("/postTasks", async (req, res) => {
   const TaskCollection = client.db("TaskCollection").collection("tasks");
   const data = req.body;
   try {
@@ -72,6 +94,55 @@ app.post("/tasks", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+// Update task by ID
+app.put("/updateTask/:id", async (req, res) => {
+  const itemId = req.params.id; 
+  const updatedTask = req.body; 
+  const TaskCollection = client.db("TaskCollection").collection("tasks");
+  // console.log(itemId,updatedTask)
+
+  try {
+    // Attempt to update the task with the specified ID
+    const result = await TaskCollection.findOneAndUpdate(
+      { _id: new ObjectId(itemId) },
+      { $set: updatedTask },
+      { returnOriginal: false }
+    );
+console.log( result.ok)
+    if (!result) {
+     
+      return res.status(404).send("Task not found");
+    }
+
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating task:", error);
+    res.status(500).send("Error updating task");
+  }
+});
+// Delete task by ID
+app.delete("/deleteTask/:id", async (req, res) => {
+  const itemId = req.params.id;
+
+  const TaskCollection = client.db("TaskCollection").collection("tasks");
+
+  try {
+   
+    const result = await TaskCollection.deleteOne({ _id: new ObjectId(itemId) });
+    console.log(result)
+    if (result.deletedCount === 0) {
+    
+      return res.status(404).send("Task not found");
+    }
+  
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    res.status(500).send("Error deleting task");
+  }
+});
+
+
 
 // Run the application
 run().catch(console.dir);
